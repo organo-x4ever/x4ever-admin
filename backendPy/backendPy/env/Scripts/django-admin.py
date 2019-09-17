edbf48f0bc2dca8545c9f0ab763eea070a702f29
Gurpreet Deol <deol.gs@hotmail.com>
2019-09-17 16:18:30 -0700

    Initial Setup

---
+++ backendPy/backendPy/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\gurpreet.deol\source\repos\x4ever-admin\backendPy\backendPy\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
